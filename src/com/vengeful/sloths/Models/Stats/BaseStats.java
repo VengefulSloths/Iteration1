5d919432392b6de3df2325e6d3a7cf22ec518124
Zach Schultz <zacharymschultz@gmail.com>
2016-01-30 13:44:42 -0500

    Added Models package, with base Entity, Occupation, and Stats packages included

---
+++ src/com/vengeful/sloths/Models/Stats/BaseStats.java
@@ -0,0 +1,11 @@
+package com.vengeful.sloths.Models.Stats;
+
+/**
+ * Created by zach on 1/30/16.
+ */
+public class BaseStats extends Stats {
+
+    public BaseStats() {
+        super();
+    }
+}
