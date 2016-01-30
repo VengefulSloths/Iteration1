8561cf4e991403cc4be82558c4ca8dafb67b7bf2
JohnnyRacket <john.randolph.harrison@gmail.com>
2016-01-30 14:01:40 -0500

    making map model

--- src/com/vengeful/sloths/Models/Map/MapItem.java
+++ src/com/vengeful/sloths/Models/Map/MapItem.java
@@ -1,4 +1,4 @@
-package com.vengeful.sloths.Model.Map;
+package com.vengeful.sloths.Models.Map;
 
 /**
  * Created by John on 1/30/2016.
--- src/com/vengeful/sloths/Models/Map/Tile.java
+++ src/com/vengeful/sloths/Models/Map/Tile.java
@@ -1,9 +1,9 @@
-package com.vengeful.sloths.Model.Map;
+package com.vengeful.sloths.Models.Map;
 
 /**
  * Created by John on 1/30/2016.
  */
 public class Tile {
 
-    
+
 }
