ae8dcd4065bed4af97bdfa3b96a3b6699f304cd0
JohnnyRacket <john.randolph.harrison@gmail.com>
2016-01-30 14:00:39 -0500

    making map model

---
+++ src/com/vengeful/sloths/Model/Map/Tile.java
@@ -0,0 +1,9 @@
+package com.vengeful.sloths.Model.Map;
+
+/**
+ * Created by John on 1/30/2016.
+ */
+public class Tile {
+
+    
+}
---
+++ src/com/vengeful/sloths/Utility/Coord.java
@@ -0,0 +1,38 @@
+package com.vengeful.sloths.Utility;
+
+/**
+ * Created by John on 1/30/2016.
+ */
+public class Coord {
+
+    private int x;
+    private int y;
+
+    public Coord(){
+        x = 0;
+        y = 0;
+    }
+
+    public Coord(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    //getters and setters
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
