5704cce57b5bea8be4776fa310bd7aa0b668d55e
Icavitt <icavitt18@gmail.com>
2016-01-30 20:10:29 -0500

    entity command factory made abstract so we can compile

--- src/com/vengeful/sloths/Models/ActionCommandFactory/EntityCommandFactory.java
+++ src/com/vengeful/sloths/Models/ActionCommandFactory/EntityCommandFactory.java
@@ -6,13 +6,13 @@ import com.vengeful.sloths.Utility.Coord;
 /**
  * Created by zach on 1/30/16.
  */
-public class EntityCommandFactory extends ActionCommandFactory {
+public abstract class EntityCommandFactory extends ActionCommandFactory {
 
     public EntityCommandFactory(Map map) {
         super(map);
     }
 
-    @Override
+    
     public MovementCommand createMovementCommand(Coord dst) {
         // @TODO: Not implemented in this iteration
         return null;
