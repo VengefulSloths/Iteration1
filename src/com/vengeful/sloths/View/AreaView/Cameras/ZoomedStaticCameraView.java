c793c2c2f4b490f868beb7447d8f0d7c06066793
Alex Smith <alexs10@ufl.edu>
2016-02-06 14:19:43 -0500

    more work on equiped area view

---
+++ src/com/vengeful/sloths/View/AreaView/Cameras/ZoomedStaticCameraView.java
@@ -0,0 +1,7 @@
+package com.vengeful.sloths.View.AreaView.Cameras;
+
+/**
+ * Created by alexs on 2/6/2016.
+ */
+public class ZoomedStaticCameraView {
+}
---
+++ src/com/vengeful/sloths/View/AreaView/CoordinateStrategies/Centered64PixelCoordinateStrategy.java
@@ -0,0 +1,7 @@
+package com.vengeful.sloths.View.AreaView.CoordinateStrategies;
+
+/**
+ * Created by alexs on 2/6/2016.
+ */
+public class Centered64PixelCoordinateStrategy {
+}
