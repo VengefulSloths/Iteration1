8bd2715029f6b2ced2a7106b813b9544de2e5405
Christiansen <echristiansen@ufl.edu>
2016-02-06 17:11:30 -0500

    added HUDView

--- src/com/vengeful/sloths/View/HUDView/HUDView.java
+++ src/com/vengeful/sloths/View/HUDView/HUDView.java
@@ -4,15 +4,19 @@ import com.vengeful.sloths.Models.Stats.Stats;
 import com.vengeful.sloths.View.Observers.StatsObserver;
 import com.vengeful.sloths.View.View;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by echristiansen on 2/5/2016.
  */
 public class HUDView extends View implements StatsObserver {
 
     public static final String backgroundImageFileName = "resources/statsBackground.jpg";
-    public static final String title = "Stats";
+    public static final String title = "Character Status";
 
     public HUDView() {
+        generateTitle(title);
         this.setBackgroundImageFileName(backgroundImageFileName);
     }
 
@@ -20,4 +24,15 @@ public class HUDView extends View implements StatsObserver {
     public void alertStatChanged(Stats stat) {
 
     }
+
+
+    public void generateTitle(String title) {
+        titleLabel = new JLabel(title);
+        Font font = new Font(titleLabel.getFont().getName(), Font.BOLD, 16);
+        titleLabel.setForeground(Color.GREEN);
+        titleLabel.setFont(font);
+        this.add(titleLabel);
+
+    }
+
 }
