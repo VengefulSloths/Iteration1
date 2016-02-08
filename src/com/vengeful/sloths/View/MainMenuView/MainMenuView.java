c7e0b7f697e5e1d2cebb303d7250d24ded271937
JohnnyRacket <john.randolph.harrison@gmail.com>
2016-02-07 23:37:15 -0500

    added things to make it pretty

--- src/com/vengeful/sloths/View/MainMenuView/MainMenuView.java
+++ src/com/vengeful/sloths/View/MainMenuView/MainMenuView.java
@@ -4,8 +4,11 @@ package com.vengeful.sloths.View.MainMenuView;
 import com.vengeful.sloths.Utility.Config;
 import com.vengeful.sloths.View.MainMenuView.Commands.MenuCommandFactory;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.util.ArrayList;
 
 /**
@@ -13,16 +16,25 @@ import java.util.ArrayList;
  */
 public class MainMenuView extends MenuView{
 
+    BufferedImage logo;
+    BufferedImage bg;
+
     private void init() {
         Config config = Config.instance();
         this.setPreferredSize(new Dimension(config.getWindowWidth(), config.getWindowHeight()));
-        this.setBackground(Color.GRAY);
+
+        //this.setBackground(Color.GRAY);
+
 
         this.verticleSpacing = 32;
         this.verticalOffset = 320;
 
         children = new ArrayList<>();
-        
+        try {
+            bg = ImageIO.read(new File("resources/Menu/grey_wallpaper.png"));
+            logo = ImageIO.read(new File("resources/Menu/game_logo.png"));
+        }catch (Exception e){}
+
 
         this.addMenuComponent("resources/Menu/NewGame");
         this.addMenuComponent("resources/Menu/LoadGame");
@@ -39,12 +51,21 @@ public class MainMenuView extends MenuView{
     public void addMenuComponent(String textPath) {
         children.add(new DefaultMenuComponent(
                 textPath,
-                Config.instance().getWindowWidth()/2-DefaultMenuComponent.WIDTH/2,
+                Config.instance().getWindowWidth()/2-DefaultMenuComponent.WIDTH/2,50+
                 this.verticalOffset +(DefaultMenuComponent.HEIGTH + verticleSpacing)*menuCounter++));
     }
 
     public MainMenuView() {
         this.init();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g){
+
+        super.paintComponent(g);
+        g.drawImage(logo,350,50, null);
+
+
     }
 
 }
