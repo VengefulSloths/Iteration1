e98b5ddaeb354c48c0a29f7c4fb8536e30510c51
Zach Schultz <zacharymschultz@gmail.com>
2016-02-08 03:05:41 -0500

    Loading on that new map

--- src/com/vengeful/sloths/Models/Map/MapItems/OneShotTest.java
+++ src/com/vengeful/sloths/Models/Map/MapItems/OneShotTest.java
@@ -7,12 +7,19 @@ import com.vengeful.sloths.Models.SaveLoad.SaveManager;
  * Created by Ian on 2/1/2016.
  */
 public class OneShotTest extends OneShotItem {
+    public OneShotTest(){
+
+    }
+
     public void interact(Entity entity){
         //System.out.Println("you've activated me");
         this.destroy = true;
     }
 
     public void saveMe(SaveManager sm, int ws) {
+        if(destroy){
+            return;
+        }
         sm.writeClassLine(ws, "OneShotTest");
         super.saveMe(sm, ws);
 
